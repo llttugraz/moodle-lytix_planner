5ac1b9a9f12ac5aed3ed76867368ebf5c8ff79fd
Guenther Moser <moser@tugraz.at>
2023-11-15 13:42:38 +0100

    change and remove tables, change plugin version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023110700; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2023111500; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2021051700; // Requires this Moodle version 3.11.
 $plugin->component = 'lytix_planner'; // Full name of the plugin.
 $plugin->dependencies = [
